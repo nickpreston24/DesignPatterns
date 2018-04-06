51fd096aea3bd6b3162e03a66dea8b15236451ee
Michael Preston <michael_n_preston@outlook.com>
2018-04-06 17:48:17 -0500

    Renamed PropertyCacheExtensions

---
+++ DesignPatterns/Extensions/PropertyCacheExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Reflection;
+
+namespace DesignPatterns
+{
+    public static partial class Extensions
+    {
+        public static ConcurrentDictionary<Type, PropertyInfo[]> Cache(this Type type)
+        {
+            PropertyCache.TryAdd(type, type.GetProperties());
+            return PropertyCache.Cache;
+        }
+    }
+}
