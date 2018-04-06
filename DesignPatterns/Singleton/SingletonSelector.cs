e0709bb9d78bf211463068c2d458da8295a14ab3
Michael Preston <michael_n_preston@outlook.com>
2018-04-06 17:48:53 -0500

    Started a Singleton (Multiton) Selector

--- DesignPatterns/Singleton/SingletonSelector.cs
+++ DesignPatterns/Singleton/SingletonSelector.cs
@@ -6,6 +6,6 @@ namespace DesignPatterns.Singleton
     public class SingletonSelector
     {
         public static ConcurrentDictionary<Type, object> _singletons = new ConcurrentDictionary<Type, object>();
-        //todo: 
+        //todo: make this into a multiton with string/type keys for selecting singletons.
     }
 }
