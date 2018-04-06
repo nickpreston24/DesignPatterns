5d6b9f49682ee5c8659f311ae8d0ebfe12d732b4
Michael Preston <michael_n_preston@outlook.com>
2018-04-06 17:48:38 -0500

    Added Pluralsight's Maybe Implemenation

---
+++ DesignPatterns/Extensions/MaybeExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DesignPatterns
+{
+    public static partial class Extensions
+    {
+        public static Maybe<T> ToMaybe<T>(this T value) where T : class => value != null
+                ? Maybe.Some(value)
+                : Maybe<T>.None;
+
+        public static Maybe<T> ToMaybe<T>(this T? nullable) where T : struct => nullable.HasValue
+                ? Maybe.Some(nullable.Value)
+                : Maybe<T>.None;
+
+        public static Maybe<string> NoneIfEmpty(this string text) => string.IsNullOrEmpty(text)
+                ? Maybe<string>.None
+                : Maybe.Some(text);
+
+        public static Maybe<T> FirstOrNone<T>(this IEnumerable<T> self) where T : class => self.FirstOrDefault().ToMaybe();
+
+        public static Maybe<T> FirstOrNone<T>(this IEnumerable<T?> self) where T : struct => self.FirstOrDefault().ToMaybe();
+    }
+}
+
