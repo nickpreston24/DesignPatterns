70655c693760e5a67074133bbc4345bb327a4eb0
Michael Preston <michael_n_preston@outlook.com>
2018-05-25 15:02:53 -0500

    Updated Maybe<T>'s namespace to System
    I'm hoping this will allow it to 'extend' that namespace.

--- Common/Classes/Maybe.cs
+++ Common/Classes/Maybe.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 
-namespace Shared
+namespace System
 {
     //from: https://www.pluralsight.com/tech-blog/maybe
     public struct Maybe<T>
