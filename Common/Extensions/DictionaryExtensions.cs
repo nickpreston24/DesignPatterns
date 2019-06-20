2a0721a25b8f8c59117cb6c5e19ef75d2829a73b
Michael Preston <michael_n_preston@outlook.com>
2019-06-20 08:48:10 -0500

    removed duplicate

--- Common/Extensions/DictionaryExtensions.cs
+++ Common/Extensions/DictionaryExtensions.cs
@@ -6,6 +6,7 @@ namespace System.Collections.Generic
     {
         //https://damieng.com/blog/2012/10/29/8-things-you-probably-didnt-know-about-csharp
         public static object GetValue<TKey>(this Dictionary<TKey, object> dictionary, TKey key) => dictionary[key];
+        public static TV GetValue<TK, TV>(this IDictionary<TK, TV> dictionary, TK key) => dictionary.TryGetValue(key, out TV value) ? value : default;
 
         //https://damieng.com/blog/2012/10/29/8-things-you-probably-didnt-know-about-csharp
         public static IEnumerable<object> GetValues<TKey>(this Dictionary<TKey, object> dictionary, params TKey[] keys) => keys.Select(key => dictionary[key]).AsEnumerable();
