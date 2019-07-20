05fa28bb4bf7d2e215c2a1f26fd203231f59a732
Michael Preston <michael.n.preston@gmail.com>
2019-07-19 21:15:44 -0500

    added a neo4j console example app

---
+++ Graphs.Console/Program.cs
@@ -0,0 +1,45 @@
+﻿using Neo4j.Driver.V1;
+using System;
+using System.Linq;
+
+namespace Graphs.Client
+{
+    class HelloWorldExample : IDisposable
+    {
+        private readonly IDriver _driver;
+
+        public HelloWorldExample(string uri, string user, string password)
+        {
+            _driver = GraphDatabase.Driver(uri, AuthTokens.Basic(user, password));
+        }
+
+        public void PrintGreeting(string message)
+        {
+            using (var session = _driver.Session())
+            {
+                var greeting = session.WriteTransaction(tx =>
+                {
+                    var result = tx.Run("CREATE (a:Greeting) " +
+                                        "SET a.message = $message " +
+                                        "RETURN a.message + ', from node ' + id(a)",
+                        new { message });
+                    return result.Single()[0].As<IRecord>();
+                });
+                Console.WriteLine(greeting);
+            }
+        }
+
+        public void Dispose()
+        {
+            _driver?.Dispose();
+        }
+
+        public static void Main()
+        {
+            using (var greeter = new HelloWorldExample("bolt://localhost:7687", "neo4j", "password"))
+            {
+                greeter.PrintGreeting("hello, world");
+            }
+        }
+    }
+}
