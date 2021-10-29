4cb4ac03c7016c34b98003e946dcd2ad7c1c626f
Mike <michael.n.preston@gmail.com>
2021-10-28 22:45:29 -0400

    Removed cruft from castle core; added .snippet files

---
+++ DesignPatterns/Clevertech-Assesment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Clevertech_Assesment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World 2.0!");
+        }
+    }
+}
